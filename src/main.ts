d5a2a1279062543c60165e582a51e367597536ae
jonas <jonas76bastard@gmail.com>
2025-08-04 02:51:26 +0200

    + | Installation pinia

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import './assets/main.css'
 import router from './router/index.ts'
 
 const app = createApp(App)
+
+// Pinia c'est un gestionnaire d'état (donc de data) pour Vue, ça me permet de stocker des données globales,
+// dans mon cas celles du jeu. Graçe a ca on peut éditer et récuperer partout sur l'app ces données
+// Je peux les rendre perssistantes au refresh et au leave mais je m'en occuperai plus tard
+const pinia = createPinia()
+app.use(pinia)
+
 app.use(router)
 app.mount('#app')
 
