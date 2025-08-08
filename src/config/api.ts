244b833b6b96302d6b8f09be9860065397cf3b82
Eliam <Texsword76@gmail.com>
2025-08-08 13:34:18 +0200

    [refactor] | simplification de la configuration de l'API en supprimant la détection automatique de l'URL de base

--- src/config/api.ts
+++ src/config/api.ts
@@ -2,21 +2,8 @@
  * Configuration des URLs de l'API
  */
 
-// Détecte automatiquement le protocole et l'host
-const getBaseUrl = (): string => {
-    // if (typeof window !== 'undefined') {
-    //   // Côté client : utilise le même protocole que la page courante
-    //   return `${window.location.protocol}//${window.location.host}`;
-    // }
-    
-    // Fallback pour le SSR ou les tests
-    return 'http://localhost/auth';
-  };
-  
-  const isLocalHost = window.location.hostname === 'localhost'
-
-  export const API_CONFIG = {
-    BASE_URL: isLocalHost ? 'https://localhost:3000/auth' : '/auth',
+export const API_CONFIG = {
+  BASE_URL: '/auth',
     ENDPOINTS: {
       AUTH: {
         REGISTER: '/register',
