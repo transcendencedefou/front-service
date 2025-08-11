bd969de06f09db027010b6e9ce2f313d994b6ac1
jonas <jonas76bastard@gmail.com>
2025-08-11 23:19:28 +0200

    Je m'en sors pas la

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,18 +1,26 @@
+import { StringMappingType } from "typescript"
+
 export class User {
     id: string
     username: string
     token: string
     twoFactorEnabled?: boolean
+    avatar?: string | null
+    banner?: string | null
 
     public constructor(
         id: string,
         username: string,
         token: string,
-        twoFactorEnabled?: boolean
+        twoFactorEnabled?: boolean,
+        avatar?: string | null,
+        banner?: string | null
     ) {
         this.username = username
         this.token = token
         this.id = id
         this.twoFactorEnabled = twoFactorEnabled
+        this.avatar = avatar ?? null
+        this.banner = banner ?? null
     }
 }
